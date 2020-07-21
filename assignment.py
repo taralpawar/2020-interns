50723045bea4dfc820425c00a0dd856da1d40a97
Taral Pawar <taralpawar@gmail.com>
2020-07-21 23:07:28 +0530

    Task T-1 commit

---
+++ assignment.py
@@ -0,0 +1,32 @@
+import json
+import collections
+import pandas as pd
+import matplotlib.pyplot as plt
+import requests
+
+f = open('data.json')
+
+data = json.load(f)
+
+rates = (data['rates'])
+
+od = collections.OrderedDict(sorted(rates.items()))
+datelist = list(od.keys())
+
+val = list(rates.values())
+
+df = pd.DataFrame(val, index=datelist)
+
+#####################################################
+############# Task 1 T-1 ############################
+#####################################################
+
+inr = (df.loc[:, 'INR'])
+inrval = (list(inr))
+
+plt.plot(datelist[:30], inrval[:30])
+
+plt.xlabel('Dates')
+plt.ylabel('Exchange rates EUR to INR')
+
+plt.show()
