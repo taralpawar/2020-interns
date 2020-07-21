3faf341b7ee0f937b462920618786767655b3f89
Taral Pawar <taralpawar@gmail.com>
2020-07-21 23:17:11 +0530

    Task T-3 Commit

--- assignment.py
+++ assignment.py
@@ -45,3 +45,24 @@ plt.ylabel('Exchange rates EUR to INR')
 
 
 plt.show()
+
+#####################################################
+############# Task 3 T-3 ############################
+#####################################################
+
+response = requests.get("https://api.exchangeratesapi.io/latest")
+data = response.json()
+latest = dict(data)
+rates = latest['rates']
+gbp = rates['GBP']
+inr = rates['INR']
+
+plt.plot(datelist[:30], inrval[:30])
+plt.plot(datelist[:30], gbpval[:30])
+plt.xlabel('Dates')
+plt.ylabel('Exchange rates EUR to INR')
+str1 = "Current value of EUR to INR is " + str(inr)
+str2 = "Current value of EUR to GBP is " + str(gbp)
+plt.text(3, 8, str1)
+plt.text(3, 13, str2)
+plt.show()
